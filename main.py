46bdbeb1869e26aa641c2c9bf1dd10f786793487
jeremyzhangsq <jeremyzhangsq@gmail.com>
2019-10-22 20:09:07 +0800

    pca similarity search & kmeans done

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+import math
+import time
 
 def read(name):
 
@@ -19,14 +23,62 @@ def read(name):
     return np.array(matrix)
 
 
-
-def pca(x,k):
-    pca = PCA(n_components=k)
-    pca.fit(x)
-    decoposeX = pca.transform(x)
+def pca(x,handout=0.1,theta=0.70):
+    pca = PCA()
+    train = x[:int(x.shape[0]*handout)]
+    pca.fit(train)
+    egenval = pca.explained_variance_ratio_
+    plt.plot([i for i in range(len(egenval))], egenval,'o-', linewidth=2, markersize=5)
+    plt.show()
+    dim = 0
+    cnt = 0
+    for val in egenval:
+        cnt += val
+        dim +=1
+        if cnt >= theta:
+            break
+    pca = PCA(n_components=dim)
+    decoposeX = pca.fit_transform(x)
     return decoposeX
+
+def pearson_def(x, y):
+    assert len(x) == len(y)
+    n = len(x)
+    assert n > 0
+    avg_x = float(sum(x)) / len(x)
+    avg_y = float(sum(y)) / len(y)
+    diffprod = 0
+    xdiff2 = 0
+    ydiff2 = 0
+    for idx in range(n):
+        xdiff = x[idx] - avg_x
+        ydiff = y[idx] - avg_y
+        diffprod += xdiff * ydiff
+        xdiff2 += xdiff * xdiff
+        ydiff2 += ydiff * ydiff
+    return diffprod / math.sqrt(xdiff2 * ydiff2)
+
+def similarUser(matrix,query):
+    q = matrix[query]
+    result = []
+    for row in matrix:
+        result.append(pearson_def(q,row))
+    return result
+
+def Kmeans(matrix):
+    kmeans = KMeans(n_clusters=10).fit(matrix)
+    return kmeans.cluster_centers_
 
 if __name__ == '__main__':
     file = './jokeRate.txt'
     tweetMatrix = read(file)
-    pcaMatrix = pca(tweetMatrix,10)
+    tick1 = time.time()
+    Kmeans(tweetMatrix)
+    # similarUser(tweetMatrix, 0)
+    tick2 = time.time()
+    print("without reduction:{}s".format(tick2-tick1))
+    pcaMatrix = pca(tweetMatrix, 10)
+    Kmeans(pcaMatrix)
+    # similarUser(pcaMatrix, 0)
+    tick3 = time.time()
+    print("pca reduction:{}s".format(tick3 - tick2))
